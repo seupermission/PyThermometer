f6d00172acbd4c9ed4502c8772294174e9177d65
Cheng Maohua <cmh@seu.edu.cn>
2017-01-17 13:18:47 +0800

    update M1

--- MonitoringWithExcel/SerialPortCOMSrv.py
+++ MonitoringWithExcel/SerialPortCOMSrv.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 
 """
+    Serial Port COM Server for MS Excel
+
+       Polled data I/O: A nonblocking call will return immediately， avoid an I/O hang.
+
+
+ License: this code is in the public domain
+
+ Author:   Cheng Tianshi
+ Email:    chengts95@seu.edu.cn
 
 """
+import time
 import serial
 import pythoncom
-
 
 class SerialPortCOMServer:
     _public_methods_=["Open","get_data","Close"]
@@ -29,12 +38,26 @@ class SerialPortCOMServer:
             if self.ser:
                 self.ser.close()
             self.ser = serial.Serial(self.port, self.baud)
+            # non-block read
+            self.ser.timeout = 0 
         except (serial.SerialException, e):
             self.connok=0
         return self.connok
 
     def get_data(self):
-        self.line = self.ser.readline().decode()
+        """
+         Polled data I/O： A nonblocking call will return immediately， avoid an I/O hang.
+        """
+        tmax=20.0
+        checking = True
+        tstart = time.time()
+        while checking:
+            # self.ser.timeout = 0 ，non-block read
+            self.line = self.ser.readline().decode() 
+            if self.line!="" or time.time() - tstart > tmax:
+                checking = False
+            else:
+                time.sleep(0.5) # wait 500 ms between checks
         return self.line 
 
     def Close(self):
